1cc5524dd498f080e0ecf661285bffcf52c34f7c
nischhas <nischhal.shrestha@metropolia.fi>
2021-11-24 11:03:20 +0200

    working on image

--- server/routes/imageRoute.js
+++ server/routes/imageRoute.js
@@ -1,2 +1,9 @@
 'use strict';
-'use strict';
+
+const express = require("express");
+const {body} = require("express-validator");
+const multer = require("multer");
+
+//TODO check for image files;
+
+
