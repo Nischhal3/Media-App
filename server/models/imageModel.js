e1a73794fa2dfdffed88d49bad3c3c3da961955f
nischhas <nischhal.shrestha@metropolia.fi>
2021-11-24 16:24:37 +0200

    added methods to imageModel

--- server/models/imageModel.js
+++ server/models/imageModel.js
@@ -6,18 +6,90 @@ const httpError = require("../utils/error");
 
 const getAllImages = async () => {
   try {
-    const [rows] = await promisePool.query("SELECT * FROM image_db");
+    const [rows] = await promisePool.query(
+      "SELECT user_db.first_name, user_db.last_name, image_title, image_description, image_file FROM image_db INNER JOIN user_db on user_db.user_id = image_db.user_id"
+    );
+    console.log("Get all cats", rows);
     return rows;
   } catch (e) {
-    console.error("Get all cat", e.message);
+    console.error("Get all cats", e.message);
     const err = httpError("Sql error", 500);
     next(err);
   }
 };
 
 const getImage = async (imageId, next) => {
-  const [rows] = await promisePool.query(
-    "SELECT image_title, image_description, image_file FROM image_db INNER JOIN user_db on user_db.user_id = image_db.user_id WHERE image_id = ?",
-    [imageId]
-  );
+  try {
+    const [rows] = await promisePool.query(
+      "SELECT user_db.first_name, user_db.last_name, image_title, image_description, image_file FROM image_db INNER JOIN user_db on user_db.user_id = image_db.user_id WHERE image_id = ?",
+      [imageId]
+    );
+    console.log("Get image by id", rows[0]);
+    return rows[0];
+  } catch (e) {
+    console.error("Get image by id", e.message);
+    const err = httpError("Sql error", 500);
+    next(err);
+  }
 };
+
+const insertImage = async (cat, next) => {
+  try {
+    const [rows] = await promisePool.query(
+      "INSERT INTO image_db ( image_title, image_description, image_file) VALUES (?,?,?)"
+    );
+    console.log("Insert image", rows);
+    return rows.affectedRows;
+  } catch (e) {
+    console.error("Insert image", e.message);
+    const err = httpError("Sql error", 500);
+    next(err);
+  }
+};
+
+const deleteImage = async (imageId, user_id, role, next) => {
+  let sql = "DELETE FROM image_db WHERE image_id = ? AND user_id = ?";
+  let params = [catId, user_id];
+
+  if (role === 0) {
+    (sql = "DELETE FROM image_db WHERE image_id = ?"), (params = [imageId]);
+  }
+
+  try {
+    const [rows] = await promisePool.execute(sql, params);
+    console.log("Delete image ", rows);
+    return rows.affectedRows === 1;
+  } catch (e) {
+    console.error("Delete image", e.message);
+    const err = httpError("Sql error", 500);
+    next(err);
+  }
+};
+
+const updateImage = async(image, next)=>{
+  let sql = "UPDATE image_db SET image_title = ?, image_description = ? WHERE image_id = ? AND user_id = ? ";
+  let params = [image.image_title, image.image_description, image.image_id, image.user_id];
+
+  if(image.role === 0){
+    (sql = "UPDATE image_db SET image_title = ?,image_description = ?, user_id = ? WHERE image_id = ?"),
+    params = [image.image_title, image.image_description,image.user_id, image.image_id];
+  }
+
+  try {
+    const [rows] = await promisePool.execute(sql, params)
+    console.log("Update image ", rows);
+    return rows.affectedRows === 1;
+  } catch (e) {
+    console.error("Update image ", e.message);
+    const err = httpError("Sql error", 500);
+    next(err);
+  }
+}
+
+module.exports = {
+  getAllImages,
+  getImage,
+  insertImage, 
+  deleteImage, 
+  updateImage
+}
