8f887918b6b6fdf2b7769b98d429834cf11eb63a
giang <nguyengiang.nchg@gmail.com>
2021-11-30 13:01:47 +0200

    resovle conflict in user route

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,34 +1,25 @@
 'use strict';
-<<<<<<< HEAD
-=======
 'use strict';
->>>>>>> main
 const express = require('express');
 const { body } = require('express-validator');
-const router = express.Router()
+const router = express.Router();
 const userController = require('../controllers/userController');
 
-router.route('/user')
-	.get(userController.user_list_get)
-<<<<<<< HEAD
-	.post(
-		body('name').isLength({ min: 3 }),
-	 	body('email').isEmail(), 
-	 	body('passwd').matches('(?=.*[A-Z]).{8,}'), 
-		userController.user_create_post)
-=======
->>>>>>> main
-	.put(
-		body('name').isLength({ min: 3 }),
-		body('email').isEmail(), 
-		body('passwd').matches('(?=.*[A-Z]).{8,}'), 
-		userController.user_update_put)
-	;
+router
+  .route('/user')
+  .get(userController.user_list_get)
+  .put(
+    body('name').isLength({ min: 3 }),
+    body('email').isEmail(),
+    body('passwd').matches('(?=.*[A-Z]).{8,}'),
+    userController.user_update_put
+  );
 
 router.get('/token', userController.checkToken);
 
-router.route('/user/:id')
-	.get(userController.user_get)
-	.delete(userController.user_delete)
+router
+  .route('/user/:id')
+  .get(userController.user_get)
+  .delete(userController.user_delete);
 
-module.exports = router
+module.exports = router;
