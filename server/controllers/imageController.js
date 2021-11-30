72702c8ff0a6d93ab8785e061e19475466fb9bc4
nischhas <nischhal.shrestha@metropolia.fi>
2021-11-30 18:38:43 +0200

    removed unwanted console logs

--- server/controllers/imageController.js
+++ server/controllers/imageController.js
@@ -18,7 +18,6 @@ const get_image_list = async (req, res) => {
 
 const get_image = async (req, res, next) => {
   const image = await getImage(req.params.imageId, next, next);
-  console.log('Image by id', image);
   if (image) {
     res.json(image);
     return;
@@ -37,7 +36,6 @@ const post_image = async (req, res, next) => {
     return;
   }
 
-  console.log('Posting images', req.file);
   if (!req.file) {
     const err = httpError('Invalid file', 400);
     next(err);
@@ -45,10 +43,8 @@ const post_image = async (req, res, next) => {
   }
 
   const user_id = req.user.user_id;
-  console.log('Post done by userID', user_id);
   const image = req.body;
   image.file = req.file.filename;
-  console.log('Image post', image);
 
   const id = await insertImage(user_id, image);
   res.json({ message: `Image added with id: ${id}` });
@@ -58,8 +54,7 @@ const delete_image = async (req, res, next) => {
   const image_id = req.params.imageId;
   const user_id = req.user.user_id;
   const role = req.user.role;
-  console.log('Image id', image_id);
-  console.log('Id ', role);
+
   const deleted = await deleteImage(image_id, user_id, role, next);
   res.json({ message: `Image deleted ${deleted}` });
 };
@@ -67,9 +62,6 @@ const delete_image = async (req, res, next) => {
 const update_image = async (req, res, next) => {
   req.body.id = req.params.imageId;
   const user_id = req.user.user_id;
-  console.log('Image ID:', req.params.imageId);
-  console.log('User ID:', user_id);
-  console.log('Update post: ', req.body);
 
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
--- server/models/imageModel.js
+++ server/models/imageModel.js
@@ -9,7 +9,6 @@ const getAllImages = async () => {
     const [rows] = await promisePool.query(
       'SELECT user_db.first_name, user_db.last_name,image_id, image_title, image_description, image_file, image_price FROM image_db INNER JOIN user_db on user_db.user_id = image_db.user_id'
     );
-    console.log('Get all images', rows);
     return rows;
   } catch (e) {
     console.error('Get all images', e.message);
@@ -24,7 +23,6 @@ const getImage = async (imageId, next) => {
       'SELECT user_db.first_name, user_db.last_name, image_title, image_description, image_file FROM image_db INNER JOIN user_db on user_db.user_id = image_db.user_id WHERE image_id = ?',
       [imageId]
     );
-    console.log('Get image by id', rows[0]);
     return rows[0];
   } catch (e) {
     console.error('Get image by id', e.message);
@@ -46,7 +44,6 @@ const insertImage = async (user_id, image, next) => {
         image.image_price,
       ]
     );
-    console.log('Insert image', rows);
     return rows.affectedRows;
   } catch (e) {
     console.error('Insert image', e.message);
@@ -65,7 +62,6 @@ const deleteImage = async (imageId, user_id, role, next) => {
 
   try {
     const [rows] = await promisePool.execute(sql, params);
-    console.log('Delete image ', rows);
     return rows.affectedRows === 1;
   } catch (e) {
     console.error('Delete image', e.message);
@@ -80,7 +76,6 @@ const updateImage = async (user_id, image, next) => {
   let params = [image.image_title, image.image_description, image.id, user_id];
   try {
     const [rows] = await promisePool.execute(sql, params);
-    console.log('Update image ', rows);
     return rows.affectedRows === 1;
   } catch (e) {
     console.error('Update image ', e.message);
