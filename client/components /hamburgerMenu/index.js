48c25b65430f40ad74248210c821051362723ba0
giang <nguyengiang.nchg@gmail.com>
2021-11-30 08:46:08 +0200

    hamburger menu for login page

--- client/components /hamburgerMenu/index.js
+++ client/components /hamburgerMenu/index.js
@@ -4,5 +4,11 @@ const navLinks = document.querySelector('.nav-links');
 hamburger.addEventListener('click', () => {
   if (navLinks.classList.contains('open')) {
     navLinks.classList.remove('open');
-  } else navLinks.classList.add('open');
+    navLinks.classList.add('close');
+    hamburger.classList.remove('hamburgerOpen');
+  } else {
+    navLinks.classList.remove('close');
+    navLinks.classList.add('open');
+    hamburger.classList.add('hamburgerOpen');
+  }
 });
