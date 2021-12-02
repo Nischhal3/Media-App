c88a71716c3ac20862b9cf4f8df23e02188236a8
giang <nguyengiang.nchg@gmail.com>
2021-12-02 09:33:26 +0200

    fix style hamburger menu for phone and add menu for sign up page

--- client/pages/signup/index.js
+++ client/pages/signup/index.js
@@ -5,16 +5,31 @@ const url = 'http://localhost:3000'; // change url when uploading to server
 const signupForm = document.querySelector('#signup-form');
 
 signupForm.addEventListener('submit', async (evt) => {
-	evt.preventDefault();
-	const data = serializeJson(addUserForm);
-	const fetchOptions = {
-	  method: 'POST',
-	  headers: {
-	    'Content-Type': 'application/json',
-	  },
-	  body: JSON.stringify(data),
-	};
-	const response = await fetch(url + '/auth/register', fetchOptions);
-	const json = await response.json();
-	alert(json.message);
-      });
+  evt.preventDefault();
+  const data = serializeJson(addUserForm);
+  const fetchOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  };
+  const response = await fetch(url + '/auth/register', fetchOptions);
+  const json = await response.json();
+  alert(json.message);
+});
+
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+  if (navLinks.classList.contains('open')) {
+    navLinks.classList.remove('open');
+    navLinks.classList.add('close');
+    hamburger.classList.remove('hamburgerOpen');
+  } else {
+    navLinks.classList.remove('close');
+    navLinks.classList.add('open');
+    hamburger.classList.add('hamburgerOpen');
+  }
+});
