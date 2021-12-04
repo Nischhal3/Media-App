d315b43f8b05894b3b1a942a767477dc64ad49be
giang <nguyengiang.nchg@gmail.com>
2021-12-04 14:47:57 +0200

    profile page web version draft

--- client/pages/profile/index.js
+++ client/pages/profile/index.js
@@ -1,17 +1,31 @@
 //Tabs selection
-const tabs = document.querySelectorAll("[ data-tab-target]");
-const tabContents = document.querySelectorAll("[data-tab-content]");
+const tabs = document.querySelectorAll('[ data-tab-target]');
+const tabContents = document.querySelectorAll('[data-tab-content]');
 
-tabs.forEach(tab =>{
-    tab.addEventListener("click", ()=>{
-        const target = document.querySelector(tab.dataset.tabTarget);
-        tabContents.forEach(tabContent =>{
-            tabContent.classList.remove("active")
-        })
-        tabs.forEach(tab =>{
-            tab.classList.remove("active")
-        })
-        target.classList.add("active");
-        tab.classList.add("active");
-    })
-})
+tabs.forEach((tab) => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.tabTarget);
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove('active');
+    });
+    tabs.forEach((tab) => {
+      tab.classList.remove('active');
+    });
+    target.classList.add('active');
+    tab.classList.add('active');
+  });
+});
+
+const menu = document.querySelector('.menu');
+const navLinks = document.querySelector('.nav-links');
+const closeMenuButton = document.querySelector('.close-menu');
+
+menu.addEventListener('click', () => {
+  navLinks.classList.add('open');
+  navLinks.classList.remove('close');
+});
+
+closeMenuButton.addEventListener('click', () => {
+  navLinks.classList.add('close');
+  navLinks.classList.remove('open');
+});
