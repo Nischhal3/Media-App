d3aa1b895a66531a16cc71a89d21c1c68440e956
Amy Mai <My.Mai@metropolia.fi>
2021-12-06 23:41:09 +0200

    complete search function

--- client/pages/collection/index.js
+++ client/pages/collection/index.js
@@ -2,6 +2,9 @@
 const url = 'http://localhost:3000'; // change url when uploading to server
 
 const collectionContent = document.getElementById('collectionContent');
+const textNotFound = document.getElementById('not-found');
+const searchCollection = document.getElementById('collection');
+
 
 const createCollectionCards = (collection) => {
   collection.forEach((item) => {
@@ -26,6 +29,29 @@ const createCollectionCards = (collection) => {
   });
 };
 
+
+const createSearchCards = (item) => {
+  const singleCollection = document.createElement('div');
+  const img = document.createElement('img');
+  const title = document.createElement('h4');
+  const line = document.createElement('hr');
+  const description = document.createElement('p');
+
+  img.src = url + '/' + item.image;
+  img.alt = item.collection_title;
+  title.innerHTML = item.collection_title;
+  description.innerHTML = item.collection_description;
+
+  singleCollection.appendChild(img);
+  singleCollection.appendChild(title);
+  singleCollection.appendChild(line);
+  singleCollection.appendChild(description);
+  singleCollection.className = 'single-collection';
+
+  searchCollection.appendChild(singleCollection);
+};
+
+
 const getCollection = async () => {
   try {
     const fetchOptions = {
@@ -34,11 +60,31 @@ const getCollection = async () => {
     const response = await fetch(url + '/collection', fetchOptions);
     const collection = await response.json();
     createCollectionCards(collection);
+    searchFunction(collection);
   } catch (e) {
     console.log(e.message);
   }
 };
 getCollection();
+
+const searchFunction = (collection) => {
+  const searchButton = document.getElementById('search-button');
+  searchButton.addEventListener('click', () => {
+    searchCollection.innerHTML = "";
+    const input = document.getElementById('search-collection').value;
+    for (let i = 0; i < collection.length; i++) {
+      const titles = (collection[i].collection_title).toLowerCase();
+      if (titles.includes(input)) {
+        createSearchCards(collection[i]);
+        collectionContent.style.display = "none";
+        searchCollection.style.display = "flex";
+      }
+      if (titles.includes(input) === false) {
+        
+      }
+    }
+  });
+};
 
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
