8f9477b89f61da25583c56914e96b444995bea3e
Amy Mai <My.Mai@metropolia.fi>
2021-12-11 11:24:30 +0200

    logout for profile oage

--- client/pages/profile/index.js
+++ client/pages/profile/index.js
@@ -200,3 +200,21 @@ const optionCreated = (collections) => {
     select.appendChild(option);
   });
 };
+
+const logOut = document.getElementById('logout');
+logOut.addEventListener('click', () => {
+  (async () => {
+    try {
+      const response = await fetch(url + '/auth/logout');
+      const json = await response.json();
+      console.log(json);
+      // remove token
+      sessionStorage.removeItem('token');
+      sessionStorage.removeItem('user');
+      alert('You have logged out');
+      location.href = '../front/index.html';
+    } catch (e) {
+      console.log(e.message);
+    }
+  })();
+})
