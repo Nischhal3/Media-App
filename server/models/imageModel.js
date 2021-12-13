96b5136baf2167fc664272588d0454b4cfc02a83
Amy Mai <My.Mai@metropolia.fi>
2021-12-13 17:35:08 +0200

    remove more console logs

--- server/models/imageModel.js
+++ server/models/imageModel.js
@@ -24,7 +24,6 @@ const getImage = async (imageId, next) => {
       'SELECT collection_db.collection_id, collection_db.collection_title, user_db.first_name, user_db.last_name, image_title, image_date, image_description, image_file FROM image_db INNER JOIN user_db on user_db.user_id = image_db.user_id INNER JOIN collection_db on collection_db.collection_id = image_db.collection_id WHERE image_db.image_id = ?',
       [imageId]
     );
-    console.log('Get image by id', rows[0]);
     return rows[0];
   } catch (e) {
     console.error('Get image by id', e.message);
@@ -39,7 +38,6 @@ const getImageByCollectionId = async (id, next) => {
       'SELECT collection_db.collection_id, user_db.first_name, user_db.last_name, image_id, image_title, image_file FROM image_db INNER JOIN user_db on user_db.user_id = image_db.user_id INNER JOIN collection_db on collection_db.collection_id = image_db.collection_id WHERE image_db.collection_id = ?',
       [id]
     );
-    console.log('Get image by id', rows[0]);
     return rows;
   } catch (e) {
     console.error('Get image by id', e.message);
@@ -62,7 +60,6 @@ const insertImage = async (user_id, image, next) => {
         image.date,
       ]
     );
-    console.log('Insert image', rows);
     return rows.affectedRows;
   } catch (e) {
     console.error('Insert image', e.message);
@@ -82,7 +79,6 @@ const deleteImage = async (imageId, user_id, role, next) => {
 
   try {
     const [rows] = await promisePool.execute(sql, params);
-    console.log('Delete image ', rows);
     return rows.affectedRows === 1;
   } catch (e) {
     console.error('Delete image', e.message);
@@ -99,7 +95,6 @@ const updateImage = async (user_id, image, next) => {
       'UPDATE image_db SET image_title = ? , image_description = ?,  collection_id = ?, image_date = ? WHERE  image_id = ? AND  user_id =  ?',
       [image.title, image.description, image.collection, image.date, image.id, user_id]
     );
-    console.log('Update image ', rows);
     return rows.affectedRows === 1;
   } catch (e) {
     console.error('Update image ', e.message);
